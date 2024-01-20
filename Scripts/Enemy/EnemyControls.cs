8bf583912cb752d2893a7baac709351ecdb05513
Janastor <mrpsbix@gmail.com>
2024-01-21 04:38:45 +0500

    whoops

--- Scripts/Enemy/EnemyControls.cs
+++ Scripts/Enemy/EnemyControls.cs
@@ -2,19 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(Rigidbody2D))]
 [RequireComponent(typeof(Enemy))]
 
 public class EnemyControls : MonoBehaviour
 {
     [SerializeField] private float _speed;
-    
-    private Rigidbody2D _rb;
-    
-    void Start()
-    {
-        _rb = GetComponent<Rigidbody2D>();
-    }
     
     void Update()
     {
